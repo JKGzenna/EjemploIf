fe83e1a2eec52b58d9224d55cafd5e1b87de6d75
JKGzenna <juanlu360@gmail.com>
2017-06-12 00:34:30 +0200

    Ejercicio Operaciones

---
+++ src/ejemploif/EjemploIf.java
@@ -0,0 +1,23 @@
+package ejemploif;
+
+/**
+ *
+ * @author JUAN
+ */
+public class EjemploIf {
+
+    public static void main(String[] args) {
+        
+      //int a = 5;
+        int a = 1;
+        
+        if( a > 2 ){
+            System.out.println("a es mayor que 2");
+        }
+        else
+        {
+            System.out.println("a es menor que 2");
+        }
+    }
+    
+}
